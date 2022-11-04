b3d7ca5bf37313f796c8c16d8bc62c50bff6692b
baijunyao <baijunyao@baijunyao.com>
2022-11-04 22:58:36 +0800

    Fix phpcs

--- src/Exceptions/LaravelRestfulException.php
+++ src/Exceptions/LaravelRestfulException.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Baijunyao\LaravelRestful\Exceptions;
 
-use Exception;
-
-class LaravelRestfulException extends Exception
+class LaravelRestfulException extends \Exception
 {
 }
